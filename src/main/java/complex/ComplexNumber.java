8aef6814152ed3a6207df9f7c554a5de18044668
nonsof <113558928+nonsof@users.noreply.github.com>
2023-10-22 21:42:24 +0300

    Create ComplexNumber.java

---
+++ src/main/java/complex/ComplexNumber.java
@@ -0,0 +1,13 @@
+package complex;
+
+public class ComplexNumber {
+    private double real;
+    private double imaginary;
+
+    public ComplexNumber(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    // Геттеры и методы для операций с комплексными числами
+}
