a9c5a9ea126200f202e6b00231a1dd08c5b04f82
nonsof <113558928+nonsof@users.noreply.github.com>
2023-10-22 21:43:43 +0300

    Create DivisionStrategy.java

---
+++ src/main/java/strategy/DivisionStrategy.java
@@ -0,0 +1,9 @@
+package strategy;
+
+import complex.ComplexNumber;
+
+public class DivisionStrategy implements CalculatorStrategy {
+    public ComplexNumber calculate(ComplexNumber num1, ComplexNumber num2) {
+        // Реализация деления
+    }
+}
