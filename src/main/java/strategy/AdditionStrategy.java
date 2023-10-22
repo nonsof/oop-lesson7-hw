c202ee2180fe884eb90ce90493d400c8b0bf815a
nonsof <113558928+nonsof@users.noreply.github.com>
2023-10-22 21:43:05 +0300

    Create AdditionStrategy.java

---
+++ src/main/java/strategy/AdditionStrategy.java
@@ -0,0 +1,9 @@
+package strategy;
+
+import complex.ComplexNumber;
+
+public class AdditionStrategy implements CalculatorStrategy {
+    public ComplexNumber calculate(ComplexNumber num1, ComplexNumber num2) {
+        // Реализация сложения
+    }
+}
